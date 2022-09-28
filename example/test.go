6c9bbbfc4b259c53e35f8f21e24b3229d698fc81
yushulx <lingxiao1002@gmail.com>
2022-09-28 15:44:43 +0800

    Update

--- example/test.go
+++ example/test.go
@@ -17,6 +17,8 @@ func main() {
 		} else {
 			fmt.Println("Input File not found! Use test.png instead.")
 		}
+	} else {
+		fmt.Println("Input File not found! Use test.png instead.")
 	}
 	ret, _ := goBarcodeQrSDK.InitLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==")
 	if ret != 0 {
