0d9c84b3290598851af3eb7870c3d0f369bbd465
yushulx <lingxiao1002@gmail.com>
2024-03-14 13:36:37 +0800

    Updated

--- example/command-line/run_linux.sh
+++ example/command-line/run_linux.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# ASSEMBLY_PATH=$(ls -d $(go env GOPATH)/pkg/mod/github.com/yushulx/*/lib)
-ASSEMBLY_PATH=../../lib/linux
-export LD_LIBRARY_PATH="$ASSEMBLY_PATH:$LD_LIBRARY_PATH"
-echo "LD_LIBRARY_PATH set to $LD_LIBRARY_PATH"
+# Save the original PATH
+originalPath=$LD_LIBRARY_PATH
 
+# Get the GOPATH
+GOPATH=$(go env GOPATH)
+
+# Find the path to the shared libraries
+PACKAGE_PATH=$(find "$GOPATH/pkg/mod/github.com/yushulx" -mindepth 1 -maxdepth 1 -type d | sort -r | head -n 1)
+echo "PACKAGE_PATH set to $PACKAGE_PATH"
+
+ASSEMBLY_PATH="$PACKAGE_PATH/lib/linux"
+echo "ASSEMBLY_PATH set to $ASSEMBLY_PATH"
+
+export LD_LIBRARY_PATH="$ASSEMBLY_PATH:$originalPath"
+
+# Run your Go application
 go run test.go test.png
+
+# Restore the original PATH
+export LD_LIBRARY_PATH=$originalPath
--- example/command-line/run_windows.ps1
+++ example/command-line/run_windows.ps1
@@ -1,9 +1,14 @@
+$originalPath = $env:PATH
+
 # Get the GOPATH
-# $GOPATH = $(go env GOPATH)
+$GOPATH = $(go env GOPATH)
 
 # Find the path to the shared libraries
-# $PACKAGE_PATH = Get-ChildItem -Path "$GOPATH\pkg\mod\github.com\yushulx" -Directory | Sort-Object LastWriteTime -Descending | Select-Object -First 1 -ExpandProperty FullName
-$ASSEMBLY_PATH = "../../lib/windows"
+$PACKAGE_PATH = Get-ChildItem -Path "$GOPATH\pkg\mod\github.com\yushulx" -Directory | Sort-Object LastWriteTime -Descending | Select-Object -First 1 -ExpandProperty FullName
+# $ASSEMBLY_PATH = "../../lib/windows"
+
+$ASSEMBLY_PATH = "$PACKAGE_PATH\lib\windows"
+
 Write-Host "ASSEMBLY_PATH set to $ASSEMBLY_PATH"
 # Update PATH to include the assembly path
 $env:PATH = "$ASSEMBLY_PATH;" + $env:PATH
@@ -11,3 +16,5 @@ $env:PATH = "$ASSEMBLY_PATH;" + $env:PATH
 
 # Run your Go application
 go run test.go test.png
+
+$env:PATH = $originalPath
