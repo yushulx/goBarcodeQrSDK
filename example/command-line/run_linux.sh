b9a2bf2e8e50e24c2993c86ffded8d972ebc1eca
yushulx <lingxiao1002@gmail.com>
2024-03-18 16:07:45 +0800

    Updated scripts for local dependencies

--- example/command-line/run_linux.sh
+++ example/command-line/run_linux.sh
@@ -9,11 +9,13 @@ GOPATH=$(go env GOPATH)
 # Find the path to the shared libraries
 PACKAGE_PATH=$(find "$GOPATH/pkg/mod/github.com/yushulx" -mindepth 1 -maxdepth 1 -type d | sort -r | head -n 1)
 echo "PACKAGE_PATH set to $PACKAGE_PATH"
+# LIBRARY_PATH="$PACKAGE_PATH/lib/linux"
 
-ASSEMBLY_PATH="$PACKAGE_PATH/lib/linux"
-echo "ASSEMBLY_PATH set to $ASSEMBLY_PATH"
+LIBRARY_PATH="../../lib/linux"
 
-export LD_LIBRARY_PATH="$ASSEMBLY_PATH:$originalPath"
+echo "LIBRARY_PATH set to $LIBRARY_PATH"
+
+export LD_LIBRARY_PATH="$LIBRARY_PATH:$originalPath"
 
 # Run your Go application
 go run test.go test.png
